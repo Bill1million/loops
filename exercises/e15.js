45b721d22d328fee19d247cf9c9860fff57fe404
william caldwell <william.eg.caldwell@gmail.com>
2024-03-04 11:08:31 -0700

    pass e15

--- exercises/e15.js
+++ exercises/e15.js
@@ -5,8 +5,17 @@
 // NOTE: You can NOT use the array.concat() method in your code
 
 export function concatArrays(arr1, arr2) {
-  // Your code goes here...
+  const resultArray = [];
 
+  for (let i = 0; i < arr1.length; i++) {
+      resultArray.push(arr1[i]);
+  }
+
+  for (let j = 0; j < arr2.length; j++) {
+      resultArray.push(arr2[j]);
+  }
+
+  return resultArray;
 }
 
 
